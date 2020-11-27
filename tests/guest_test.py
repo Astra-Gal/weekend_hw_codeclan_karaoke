4c20daf2daae439ea83bd1fdd773a1a995077300
Astra-Gal <giulia.galastro@gmail.com>
2020-11-27 19:50:14 +0000

    eight tests passing

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -10,3 +10,12 @@ class TestGuest(unittest.TestCase):
 
     def test_guest_has_name(self):
         self.assertEqual("Shane MacGowan", self.guest.name)
+
+    def test_guest_has_age(self):
+        self.assertEqual(62, self.guest.age)
+
+    def test_guest_has_wallet(self):
+        self.assertEqual(100.00, self.guest.wallet)
+
+    def test_guest_has_skill_at_singing_out_of_ten(self):
+        self.assertEqual(3, self.guest.skill_at_singing_out_of_ten)
