7b54671894d6b7d0c54c8bdf13624c76a78910a7
Astra-Gal <giulia.galastro@gmail.com>
2020-11-27 18:31:02 +0000

    four song tests passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -4,13 +4,16 @@ from src.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        self.song = Song("The Parting Glass", "The Pogues", 2.17)
+        self.song = Song("The Parting Glass", "The Pogues", "Messy Folk-Punk", 2.17)
 
     def test_song_has_title(self):
         self.assertEqual("The Parting Glass", self.song.title)
 
     def test_song_has_artist(self):
         self.assertEqual("The Pogues", self.song.artist)
+
+    def test_song_has_genre(self):
+        self.assertEqual("Messy Folk-Punk", self.song.genre)
 
     def test_song_has_duration(self):
         self.assertEqual(2.17, self.song.duration)
