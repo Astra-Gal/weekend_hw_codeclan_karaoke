3d72f9aeef5dcc2c3bd70a36bf66920d12a8f16d
Astra-Gal <giulia.galastro@gmail.com>
2020-11-27 18:43:54 +0000

    first guest test passing

---
+++ src/guest.py
@@ -0,0 +1,8 @@
+class Guest:
+
+    def __init__(self, name, age, wallet, skill_at_singing_out_of_ten):
+        self.name = name
+        self.age = age
+        self.wallet = wallet
+        self.skill_at_singing_out_of_ten = skill_at_singing_out_of_ten
+        
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,7 +1,12 @@
 import unittest
 
-# from src.guest import Guest 
-# from src.song import Song 
+from src.guest import Guest 
+from src.song import Song 
 
-# class TestGuest(unittest.TestCase):
+class TestGuest(unittest.TestCase):
 
+    def setUp(self):
+        self.guest = Guest("Shane MacGowan", 62, 100.00, 3)
+
+    def test_guest_has_name(self):
+        self.assertEqual("Shane MacGowan", self.guest.name)
