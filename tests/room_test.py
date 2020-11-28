67560507e50dab85b1748c1240a485df1c220f38
Astra-Gal <giulia.galastro@gmail.com>
2020-11-28 12:08:57 +0000

    first room test passed

--- tests/room_test.py
+++ tests/room_test.py
@@ -1,8 +1,12 @@
 import unittest
 
-# from src.room import Room
-# from src.guest import Guest 
-# from src.song import Song 
+from src.room import Room
+from src.guest import Guest 
+from src.song import Song 
 
-# class TestRoom(unittest.TestCase):
-    
+class TestRoom(unittest.TestCase):
+    def setUp(self):
+        self.room_1 = Room("Wannabe", 10)
+
+    def test_room_has_name(self):
+        self.assertEqual("Wannabe", self.room_1.name)
